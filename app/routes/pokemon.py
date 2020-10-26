a460a4a47826b1d7128283caa881fe291b9a7df3
jeckscolari <giacomo.scolari92@gmail.com>
2020-10-26 18:52:02 +0100

    Add errors on doc for pokemon endpoint

--- app/routes/pokemon.py
+++ app/routes/pokemon.py
@@ -10,6 +10,9 @@ router = APIRouter()
 @router.get(
     '/{pokemon_name}', 
     response_model=PokemonResponse,
-    name='pokemon:get-shakespearean-description')
+    name='pokemon:get-shakespearean-description',
+    responses={
+        404: {'description': 'Pokemon not found'},
+        502: {'description': 'PokeAPI or Shakespeare translator API error'}})
 async def get_shakespearean_description(pokemon_name: str, pokemon_service: PokemonService = Depends(PokemonService)) -> PokemonResponse:
     return await pokemon_service.get_description(pokemon_name=pokemon_name)
